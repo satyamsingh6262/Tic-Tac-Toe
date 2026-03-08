3624687516ca1d9ca7ea6698610ca2c1082621b4
Satyam Singh <satyamsingh62jnp@gmail.com>
2026-03-08 13:39:14 +0530

    Add reset and undo functionality to TicTacToe

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,21 +5,41 @@ import java.awt.event.*;
 public class TicTacToe extends JFrame implements ActionListener {
 
     JButton[] buttons = new JButton[9];
+    JButton resetButton, undoButton;
+
     boolean playerX = true;
+    int lastMove = -1;
 
     public TicTacToe() {
 
         setTitle("Tic Tac Toe");
-        setSize(400,400);
-        setLayout(new GridLayout(3,3));
+        setSize(400,500);
+        setLayout(new BorderLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel board = new JPanel();
+        board.setLayout(new GridLayout(3,3));
 
         for(int i=0;i<9;i++){
             buttons[i] = new JButton("");
             buttons[i].setFont(new Font("Arial",Font.BOLD,60));
             buttons[i].addActionListener(this);
-            add(buttons[i]);
+            board.add(buttons[i]);
         }
+
+        JPanel controlPanel = new JPanel();
+
+        resetButton = new JButton("Reset");
+        undoButton = new JButton("Undo");
+
+        resetButton.addActionListener(e -> resetBoard());
+        undoButton.addActionListener(e -> undoMove());
+
+        controlPanel.add(resetButton);
+        controlPanel.add(undoButton);
+
+        add(board, BorderLayout.CENTER);
+        add(controlPanel, BorderLayout.SOUTH);
 
         setVisible(true);
     }
@@ -28,17 +48,21 @@ public class TicTacToe extends JFrame implements ActionListener {
 
         JButton clicked = (JButton)e.getSource();
 
-        if(clicked.getText().equals("")){
+        for(int i=0;i<9;i++){
 
-            if(playerX){
-                clicked.setText("X");
-                playerX = false;
-            } else {
-                clicked.setText("O");
-                playerX = true;
+            if(clicked == buttons[i] && buttons[i].getText().equals("")){
+
+                if(playerX){
+                    buttons[i].setText("X");
+                    playerX = false;
+                } else {
+                    buttons[i].setText("O");
+                    playerX = true;
+                }
+
+                lastMove = i;
+                checkWinner();
             }
-
-            checkWinner();
         }
     }
 
@@ -56,23 +80,59 @@ public class TicTacToe extends JFrame implements ActionListener {
                buttons[w[0]].getText().equals(buttons[w[1]].getText()) &&
                buttons[w[1]].getText().equals(buttons[w[2]].getText())){
 
+                // highlight winning row
+                buttons[w[0]].setBackground(Color.GREEN);
+                buttons[w[1]].setBackground(Color.GREEN);
+                buttons[w[2]].setBackground(Color.GREEN);
+
                 JOptionPane.showMessageDialog(this,
-                        buttons[w[0]].getText()+" Wins!");
+                        buttons[w[0]].getText() + " Wins!");
 
                 resetBoard();
                 return;
             }
         }
+
+        boolean draw = true;
+
+        for(JButton b : buttons){
+            if(b.getText().equals("")){
+                draw = false;
+                break;
+            }
+        }
+
+        if(draw){
+            JOptionPane.showMessageDialog(this,"Match Draw!");
+            resetBoard();
+        }
+    }
+
+    void undoMove(){
+
+        if(lastMove != -1){
+
+            buttons[lastMove].setText("");
+
+            playerX = !playerX;
+
+            lastMove = -1;
+        }
     }
 
     void resetBoard(){
+
         for(JButton b : buttons){
             b.setText("");
+            b.setBackground(null);
         }
+
         playerX = true;
+        lastMove = -1;
     }
 
     public static void main(String[] args) {
+
         new TicTacToe();
     }
 }
